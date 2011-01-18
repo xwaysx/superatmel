d04c131a6d0119108069de232a291219e2ff163e
Sakae <juan@mac-sa.es>
2011-01-18 16:12:27 +0100

    Subido base

---
+++ vendorsetup.sh
@@ -0,0 +1,11 @@
+add_lunch_combo superatmel_bravo-eng
+#add_lunch_combo superatmel_buzz-eng
+#add_lunch_combo superatmel_dream_sapphire-eng
+#add_lunch_combo superatmel_hero-eng
+#add_lunch_combo superatmel_legend-eng
+add_lunch_combo superatmel_one-eng
+add_lunch_combo superatmel_passion-eng
+add_lunch_combo superatmel_z71-eng
+
+PATH=$PATH:$PWD/vendor/superatmel/tools ; export PATH
+VENDOR=$PWD/vendor/superatmel
