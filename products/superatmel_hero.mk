89a691832bcf1aeec1e5df7282a10cf524a9d177
blerfont <blerfont@gmail.com>
2011-01-22 20:27:28 +0100

    eliminado prelink y editado product hero ya que da problemas en la compilación, añadidos hero y wildfire al combo

--- products/superatmel_hero.mk
+++ products/superatmel_hero.mk
@@ -9,14 +9,14 @@ PRODUCT_BRAND := htc
 PRODUCT_DEVICE := hero
 PRODUCT_MODEL := Hero
 PRODUCT_MANUFACTURER := HTC
-PRODUCT_BUILD_PROP_OVERRIDES += BUILD_ID=FRG83 BUILD_DISPLAY_ID=FRG83 BUILD_FINGERPRINT=google/passion/passion/mahimahi:2.2.1/FRG83/60505:user/release-keys PRIVATE_BUILD_DESC="passion-user 2.2.1 FRG83 60505 release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += BUILD_ID=FRG83 BUILD_DISPLAY_ID=GRH78C BUILD_FINGERPRINT=google/passion/passion/mahimahi:2.2.1/FRG83/60505:user/release-keys PRIVATE_BUILD_DESC="passion-user 2.2.1 FRG83 60505 release-keys"
 
-PRODUCT_SPECIFIC_DEFINES += TARGET_PRELINKER_MAP=$(TOP)/vendor/superatmel/prelink-linux-arm-hero.map
+# PRODUCT_SPECIFIC_DEFINES += TARGET_PRELINKER_MAP=$(TOP)/vendor/superatmel/prelink-linux-arm-hero.map
 
 WITH_WINDOWS_MEDIA := true
 
 # Extra Passion overlay
-PRODUCT_PACKAGE_OVERLAYS += vendor/superatmel/overlayazul/hero
+PRODUCT_PACKAGE_OVERLAYS += vendor/superatmel/overlay/hero
 
 # Build kernel
 PRODUCT_SPECIFIC_DEFINES += TARGET_PREBUILT_KERNEL=
@@ -25,7 +25,7 @@ PRODUCT_SPECIFIC_DEFINES += TARGET_KERNEL_CONFIG=superbler_hero_defconfig
 
 # Set ro.modversion
 PRODUCT_PROPERTY_OVERRIDES += \
-	ro.modversion=SuperBler-MVZ-Hero-4
+	ro.modversion=SuperTeam-SBC-Hero-1
 
 # Copy specific prebuilt files
 #
