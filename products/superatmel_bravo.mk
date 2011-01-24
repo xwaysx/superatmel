21fd7644b6c3b335555fb77f98fa64a7bb1ef5d2
Sakae <juan@mac-sa.es>
2011-01-24 14:59:09 +0100

    añadido Desire
    Change-Id: Ib41496729a0105585804c45f28834bf708e3a539

--- products/superatmel_bravo.mk
+++ products/superatmel_bravo.mk
@@ -2,7 +2,7 @@
 $(call inherit-product, device/htc/bravo/full_bravo.mk)
 
 # Inherit some common cyanogenmod stuff.
-$(call inherit-product, vendor/superatmel/products/common.mk)
+$(call inherit-product, vendor/superatmel/products/common_full.mk)
 
 # Include GSM stuff
 $(call inherit-product, vendor/superatmel/products/gsm.mk)
@@ -15,19 +15,23 @@ PRODUCT_BRAND := htc_wwe
 PRODUCT_DEVICE := bravo
 PRODUCT_MODEL := HTC Desire
 PRODUCT_MANUFACTURER := HTC
-PRODUCT_BUILD_PROP_OVERRIDES += BUILD_ID=FRF91 BUILD_DISPLAY_ID=FRG83 PRODUCT_NAME=htc_bravo BUILD_FINGERPRINT=htc_wwe/htc_bravo/bravo/bravo:2.2/FRF91/226611:user/release-keys TARGET_BUILD_TYPE=userdebug BUILD_VERSION_TAGS=release-keys PRIVATE_BUILD_DESC="2.10.405.2 CL226611 release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += BUILD_ID=FRF91 BUILD_DISPLAY_ID=GRH78C PRODUCT_NAME=htc_bravo BUILD_FINGERPRINT=htc_wwe/htc_bravo/bravo/bravo:2.2/FRF91/226611:user/release-keys TARGET_BUILD_TYPE=userdebug BUILD_VERSION_TAGS=release-keys PRIVATE_BUILD_DESC="2.10.405.2 CL226611 release-keys"
 
 # Build kernel
 PRODUCT_SPECIFIC_DEFINES += TARGET_PREBUILT_KERNEL=
 PRODUCT_SPECIFIC_DEFINES += TARGET_KERNEL_DIR=kernel-msm
 PRODUCT_SPECIFIC_DEFINES += TARGET_KERNEL_CONFIG=cyanogen_bravo_defconfig
 
-# Include the Torch and FM apps
-PRODUCT_PACKAGES += Torch \
-    FM
+# Incluye aplicaciones
+PRODUCT_PACKAGES += FM \
+   Stk \
+   Torch
+
+# Broadcom FM radio
+$(call inherit-product, vendor/cyanogen/products/bcm_fm_radio.mk)
 
 # Extra Passion overlay
-PRODUCT_PACKAGE_OVERLAYS += vendor/superatmel/overlayazul/bravo
+PRODUCT_PACKAGE_OVERLAYS += vendor/superatmel/overlay/bravo
 
 # Extra RIL settings
 PRODUCT_PROPERTY_OVERRIDES += \
@@ -40,7 +44,7 @@ WITH_WINDOWS_MEDIA := true
 
 # Set ro.modversion
 PRODUCT_PROPERTY_OVERRIDES += \
-    ro.modversion=SuperGinger-SBC-Desire-1
+    ro.modversion=SuperGinger-SBC-Desire-2.1
 
 #
 # Copy Passion specific prebuilt files
